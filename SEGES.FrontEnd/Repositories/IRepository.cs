9eef44328374377383ae0bf5c55edbd500a1bdc6
MelissaArcila <restrepo.melissa@gmail.com>
2024-05-13 20:32:34 -0500

    corrected irepo problem

--- SEGES.FrontEnd/Repositories/IRepository.cs
+++ SEGES.FrontEnd/Repositories/IRepository.cs
@@ -4,6 +4,8 @@
     {
         Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
 
+        Task<HttpResponseWrapper<object>> GetAsync(string url);
+
         Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
 
         Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
@@ -13,5 +15,6 @@
         Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
 
         Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model);
+
     }
 }
