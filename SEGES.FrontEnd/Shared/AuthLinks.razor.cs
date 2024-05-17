8b3afffc3487fa701bd423f934069b6c760e3444
IsauraEckar <isauraEckar@gmail.com>
2024-05-16 20:26:17 -0500

    Modify styles

--- SEGES.FrontEnd/Shared/AuthLinks.razor.cs
+++ SEGES.FrontEnd/Shared/AuthLinks.razor.cs
@@ -9,25 +9,31 @@ namespace SEGES.FrontEnd.Shared
     public partial class AuthLinks
     {
         private string? photoUser;
+        private string? userName;
 
         [CascadingParameter]
         private Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
-        [CascadingParameter] IModalService Modal { get; set; } = default!;
 
         protected override async Task OnParametersSetAsync()
         {
             var authenticationState = await AuthenticationStateTask;
-            var claims = authenticationState.User.Claims.ToList();
-            var photoClaim = claims.FirstOrDefault(x => x.Type == "Photo");
-            if (photoClaim is not null)
+            var user = authenticationState.User;
+
+            if (user.Identity != null && user.Identity.IsAuthenticated)
             {
-                photoUser = photoClaim.Value;
+                userName = user.Identity.Name;
+
+                var claims = user.Claims.ToList();
+                var photoClaim = claims.FirstOrDefault(x => x.Type == "Photo");
+                if (photoClaim is not null)
+                {
+                    photoUser = photoClaim.Value;
+                }
             }
         }
         private void ShowModal()
         {
-            //Modal.Show<Login>();
-            DialogService.OpenAsync<Login>("Login Modal");
+            DialogService.OpenAsync<Login>("Iniciar Sesión");
         }
     }
 }
